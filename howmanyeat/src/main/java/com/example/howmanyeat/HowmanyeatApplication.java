6dc695a4a88d98e07dc7b977de8f39bf2f13ddb4
jjackdduke <tjgmldus100@gmail.com>
2023-11-22 00:51:24 +0900

    프로젝트 생성(java11)

---
+++ howmanyeat/src/main/java/com/example/howmanyeat/HowmanyeatApplication.java
@@ -0,0 +1,13 @@
+package com.example.howmanyeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowmanyeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowmanyeatApplication.class, args);
+    }
+
+}
---
+++ howmanyeat/src/test/java/com/example/howmanyeat/HowmanyeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.howmanyeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowmanyeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
