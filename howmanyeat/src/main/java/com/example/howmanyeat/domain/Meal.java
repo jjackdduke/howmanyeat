466f38baf0028a8e97b1ebe1bea53b85c74764a4
jjackdduke <tjgmldus100@gmail.com>
2024-01-02 23:05:27 +0900

    주석 추가

--- howmanyeat/src/main/java/com/example/howmanyeat/domain/Meal.java
+++ howmanyeat/src/main/java/com/example/howmanyeat/domain/Meal.java
@@ -17,6 +17,11 @@ public class Meal {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    // 즉시로딩하면 성능튜닝이 매우 어려워진다.
+    // 성능 최적화가 꼭 필요한 경우엔 fetch join을 사용해라.
+    // 지연로딩이면 new해서 멤버 객체를 가져오는게 아니라
+    // (DB에서 가져오는게 아니라) 프록시 멤버 객체를 생성해서 넣어놓는다.
+    // 멤버 객체를 실질적으로 사용할 때 SQL을 날려서 DB에서 조회를 한다.
     @JoinColumn(name = "member_id")
     private Member member;
 
